f80c77980435550c23e15d99249e09ec04467bed
Ma-Ramirez680 <mariard@go.ugr.es>
2025-06-17 12:45:46 +0200

    figura de prueba

--- figuras.R
+++ figuras.R
@@ -1,1 +1,9 @@
 # Script para figuras
+
+library(tidyverse)
+a<-ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  labs(title = "Scatterplot of MPG vs Weight",
+       x = "Weight (1000 lbs)",
+       y = "Miles per Gallon (MPG)") +
+  theme_minimal()
