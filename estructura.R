77b9a0cba872c74bee2a9a27159fa9f04e7491f1
Bertaruizlorenzana <bertaruiz@correo.ugr.es>
2025-06-17 12:24:59 +0200

    prueba berta

--- estructura.R
+++ estructura.R
@@ -1,4 +1,4 @@
-##ESTRUCTURA DEL INFORME DE ESTADÍSTICAS DE LA PAM
+##ESTRUCTURA DEL INFORME DE ESTADÍSTICAS DE LA PAM !!!!!!!!!
 
 #1. Estadísticas nacionales generales
 #1.1. Estadísticas regionales generales
