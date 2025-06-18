a33ecf2c3f313796ea23525ddf459a7418628f39
Maria <mariabelher@correo.ugr.es>
2025-06-18 12:22:58 +0200

    arreglos menores parte maria B

--- estructura_compartida/maria_b.R
+++ estructura_compartida/maria_b.R
@@ -1,40 +1,41 @@
 #7. REVOCACIONES DE LA PAM
-## Revocaciones por comunidad autónoma y sexo
-## Revocaciones por enfermedad de base y año 2021-24
-## Revocaciones por tramo de edad
-## Revocaciones por fase del proceso
-## Tasa solicitudes/revocaciones
+## Revocaciones por comunidad autónoma y sexo (excel 14/a mano)
+## Revocaciones por enfermedad de base y año 2021-24 (a mano)
+## Revocaciones por tramo de edad (a mano)
+## Revocaciones por fase del proceso (a mano)
+## Tasa solicitudes/revocaciones (a mano)
 
 
 
 #8. PLAZOS Y PROCEDIMIENTOS DE LA TRAMITACIÓN
 #8.1. ROL DE LOS PROFESIONALES SANITARIOS: MC Y MR
-## Especialidad del MR y comunidad autónoma
-## Especialidad del MC y comunidad autónoma
+## Especialidad del MR y comunidad autónoma (excel 5)
+## Especialidad del MC y comunidad autónoma (excel 7)
 
 #8.2. DATOS SOBRE OBJECIÓN DE CONCIENCIA
 
 #8.3. TIEMPOS DEL PROCEDIMIENTO
-## Media y mediana de tiempo entre 1ª solicitud y 2ª solicitud por años 2021-24.
-## Media y mediana de tiempo entre 2ª solicitud e informe MC por años 2021-24 (borrador).
-## Media y mediana de tiempo entre informe MC y resolución de CGyE por años 2021-24 (borrador).
-## Media y mediana de tiempo entre resolución de CGyE y la prestación por años 2021-24 (borrador).
-## Media y mediana de tiempo entre la reclamación a la CGyE y la respuesta por años 2021-24 (borrador).
+## Media y mediana de tiempo entre 1ª solicitud hasta que se autoriza la prestación por parte de la CGyE por años 2021-24 (excel 15+19).
+## Media y mediana de tiempo entre 1ª solicitud y 2ª solicitud por años 2021-24 (excel 16)
+## Media y mediana de tiempo entre 2ª solicitud e informe MC por años 2021-24 (excel 17) .
+## Media y mediana de tiempo entre informe MC y resolución de CGyE por años 2021-24 (excel 18).
+## Media y mediana de tiempo entre resolución de CGyE y la prestación por años 2021-24 (excel 19).
+## Media y mediana de tiempo entre la reclamación a la CGyE y la respuesta por años 2021-24 (excel 20).
 
 #8.4. APLAZAMIENTOS DE LA PRESTACIÓN
-## Aplazamientos por comunidad autónoma
-## Aplazamientos por años 2021-24
-## Aplazamientos por tramos de edad
-## Aplazamientos por sexo
-## Aplazamientos por enfermedad de base
-## Aplazamientos por momento del trámite????
-## Tasas de aplazamientos por solicitud aceptada por CGyE.
-## Tasa de aplazamientos por solicitudes totales por C.A. 
+## Aplazamientos por comunidad autónoma (excel 13)
+## Aplazamientos por años 2021-24 (a mano)
+## Aplazamientos por tramos de edad (a mano)
+## Aplazamientos por sexo (a mano)
+## Aplazamientos por enfermedad de base (a mano)
+## Aplazamientos por momento del trámite ?????
+## Tasas de aplazamientos por solicitud aceptada por CGyE (a mano)
+## Tasa de aplazamientos por solicitudes totales por C.A (a mano)
 
 
 
 #9. ESTADÍSTICAS RELACIONADAS CON LA DONACIÓN DE ÓRGANOS POST-PAM (sintetizar de forma vistosa)
-## Frecuencia de donantes tras la PAM por años 2021-24 por C.A. (disponible en borrador).
+## Frecuencia de donantes tras la PAM por años 2021-24 por C.A. (excel 21).
 ## Frecuencia de órganos extraídos tras la PAM por años 2021-24 por C.A. (disponible en borrador).????
 ## Frecuencia de órganos trasplantados tras la PAM por años 2021-24 por C.A. (disponible en borrador).????
 ## Frecuencia de personas trasplantadas tras la PAM por años 2021-24 por C.A. (disponible en borrador).????
