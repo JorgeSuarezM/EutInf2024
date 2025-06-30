b3c1c31a2b0d17fb0619f5384e41daac9e3a42b9
Jorge Suarez <suarezmjorge@gmail.com>
2025-06-30 14:41:42 +0200

    cambios en prep.R (PUEDE HABER CONFLICTOS, LEER COMENTARIOS) + generar tabla ejemplo en scrip_tablas

--- prep.R
+++ prep.R
@@ -3,9 +3,10 @@
 ## Librerías
 library(tidyverse)
 library(gt)
+library(gtExtras)
 
 # Cargar los datos
-df<-read.csv("data.csv", fileEncoding="latin1", sep=";")
+df<-read.csv("data.csv", fileEncoding="UTF-8", sep=";")
 glimpse(df)
 
 # Establecer formato de fecha
@@ -18,22 +19,64 @@ Fecha.Informe.favorable.m.dico.responsable = as.Date(Fecha.Informe.favorable.m.d
 Fecha.de.Fallecimiento = as.Date(Fecha.de.Fallecimiento, format="%d/%m/%y"),
 Fecha.prestacion = as.Date(FECHA_PRESTACION, format="%d/%m/%y"))
 
+# Renombrar columnas para facilitar el uso
+# (!) Comentar con ## las líneas siguientes para evitar conflictos con las tablas ya hechas
+df<-df %>%
+rename(
+    InformeMR = Informe.favorable..médico.responsable,
+    InformeMC = Informe.favorable.médico.consultor,
+    InformeCGyE = Informe.favorable.Comisión.Garantía.y.Evaluación..CGyE.,
+    FechaCGyE = Fecha.Informe.favorable.Comisión.Garantía.y.Evaluación..CGyE.,
+    FechaMC = Fecha.Informe.favorable.médico.consultor,
+    FechaMR = Fecha.Informe.favorable.médico.responsable,
+    FechaFallecimiento = Fecha.de.Fallecimiento,
+    FechaPrestacion = FECHA_PRESTACION,
+    FechaCierre = FECHA_CIERRE
+)
+
+# Renombrar CCAA
+df<-df%>%
+mutate(CCAAor = CCAA,
+    CCAA = case_when(
+    CCAA == "ANDALUCÍA" ~ "Andalucía",
+    CCAA == "ARAGÓN" ~ "Aragón",
+    CCAA == "ASTURIAS, PRINCIPADO DE" ~ "Asturias",
+    CCAA == "BALEARS, ILLES" ~ "Illes Balears",
+    CCAA == "CANARIAS" ~ "Canarias",
+    CCAA == "CANTABRIA" ~ "Cantabria",
+    CCAA == "CASTILLA Y LEÓN" ~ "Castilla y León",
+    CCAA == "CASTILLA-LA MANCHA" ~ "Castilla-La Mancha",
+    CCAA == "CATALUÑA" ~ "Cataluña",
+    CCAA == "COMUNITAT VALENCIANA" ~ "C. Valenciana",
+    CCAA == "EXTREMADURA" ~ "Extremadura",
+    CCAA == "GALICIA" ~ "Galicia",
+    CCAA == "MADRID, COMUNIDAD DE" ~ "Madrid",
+    CCAA == "MURCIA, REGIÓN DE" ~ "Murcia",
+    CCAA == "NAVARRA, COMUNIDAD FORAL DE" ~ "Navarra",
+    CCAA == "PAIS VASCO" ~ "País Vasco",
+    CCAA == "RIOJA, LA" ~ "La Rioja",
+    CCAA == "CEUTA" ~ "Ceuta",
+    CCAA == "MELILLA" ~ "Melilla",
+    CCAA == "" | is.na(CCAA) ~ "No consta"
+))
+
 # Crear variables de denegación y muerte durante la tramitación
 df<-df %>%
 mutate(Denegado=if_else(
-    (Informe.favorable..m.dico.responsable == 0 | Informe.favorable.m.dico.consultor == 0 | Informe.favorable.Comisi.n.Garant.a.y.Evaluaci.n..CGyE. == 0) & is.na(FECHA_PRESTACION), 1, 0),
-    muerte.durante.tramitacion=case_when(
-        Fecha.de.Fallecimiento > Fecha.Informe.favorable.Comisi.n.Garant.a.y.Evaluaci.n..CGyE. ~ 0,
-        (Fecha.de.Fallecimiento <= Fecha.Informe.favorable.Comisi.n.Garant.a.y.Evaluaci.n..CGyE. | Fecha.de.Fallecimiento <=Fecha.Informe.favorable.m.dico.consultor | Fecha.de.Fallecimiento <= Fecha.Informe.favorable.m.dico.responsable) ~ 1,
-        !is.na(Fecha.de.Fallecimiento) & is.na(Fecha.Informe.favorable.m.dico.responsable) ~ 1
+    (InformeMR == 0 | InformeMC == 0 | InformeCGyE == 0) & FechaPrestacion!="", 1, 0),
+    MuerteDuranteTramitacion=case_when(
+        FechaFallecimiento > FechaCGyE ~ 0,
+        FechaPrestacion!= "" ~ 0,
+        (FechaFallecimiento <= FechaCGyE | FechaFallecimiento <= FechaMC | FechaFallecimiento <= FechaMR) ~ 1,
+        !is.na(FechaFallecimiento) & (is.na(FechaPrestacion)|FechaPrestacion=="") ~ 1
     ),
-    muerte.durante.tramitacion.momento=case_when(
-        muerte.durante.tramitacion == 0 ~ "No",
-        Fecha.de.Fallecimiento <= Fecha.Informe.favorable.Comisi.n.Garant.a.y.Evaluaci.n..CGyE. ~ "1Previo a Comisión",
-        Fecha.de.Fallecimiento <= Fecha.Informe.favorable.m.dico.consultor ~ "2Previo a Informe Médico Consultor",
-        Fecha.de.Fallecimiento <= Fecha.Informe.favorable.m.dico.responsable ~ "3Previo a Informe Médico Responsable"
+    MuerteDuranteTramitacionMomento=case_when(
+        MuerteDuranteTramitacion == 0 ~ "No",
+        FechaFallecimiento <= FechaCGyE ~ "1Previo a Comisión",
+        FechaFallecimiento <= FechaMC ~ "2Previo a Informe Médico Consultor",
+        FechaFallecimiento <= FechaMR ~ "3Previo a Informe Médico Responsable"
     )
 )
 
 # Verificar si la fecha de prestación es anterior a la fecha del informe favorable de la comisión
-table(as.Date(df$FECHA_PRESTACION, format="%d/%m/%y") < as.Date(df$Fecha.Informe.favorable.Comisi.n.Garant.a.y.Evaluaci.n..CGyE., format="%d/%m/%y"))
+table(as.Date(df$FechaPrestacion, format="%d/%m/%y") < as.Date(df$FechaCGyE, format="%d/%m/%y")) # 1 caso: CODIGO NE00001484
--- script_tablas/script_tablas.R
+++ script_tablas/script_tablas.R
@@ -1,1 +1,114 @@
 ##En este script se van a agrupar las tablas al final para facilitar su exportación
+
+# Apartado 3: Estadísticas generales sobre la PAM
+## Perfil de solicitantes
+### Tabla 1: Distribución por sexo de solicitantes y beneficiarios
+t<-df %>%
+mutate(Solicitante=1,
+Beneficiario=if_else(FechaPrestacion!="", 1, 0))
+
+tab1<-t %>%
+group_by(CCAA) %>%
+summarise(
+  TotalSolicitantesN = sum(Solicitante),
+  TotalSolicitantesP = round((sum(Solicitante) / nrow(t) * 100), digits=1),
+  TotalPrestacionesN = sum(Beneficiario),
+  TotalPrestacionesP = round((sum(Beneficiario) / nrow(t) * 100), digits=1),
+  TotalMuerteDuranteTramitacionN = sum(MuerteDuranteTramitacion),
+  TotalMuerteDuranteTramitacionP = round((sum(MuerteDuranteTramitacion) / nrow(t) * 100), digits=1),
+  TotalDenegadosN = sum(Denegado, na.rm = TRUE),
+  TotalDenegadosP = round((sum(Denegado, na.rm = TRUE) / nrow(t) * 100), digits=1)
+)
+tab1b<-t %>%
+summarise(
+    CCAA = "Total",
+    TotalSolicitantesN = sum(Solicitante),
+    TotalSolicitantesP = round((sum(Solicitante) / nrow(t) * 100), digits=1),
+    TotalPrestacionesN = sum(Beneficiario),
+    TotalPrestacionesP = round((sum(Beneficiario) / nrow(t) * 100), digits=1),
+    TotalMuerteDuranteTramitacionN = sum(MuerteDuranteTramitacion),
+    TotalMuerteDuranteTramitacionP = round((sum(MuerteDuranteTramitacion) / nrow(t) * 100), digits=1),
+    TotalDenegadosN = sum(Denegado, na.rm = TRUE),
+    TotalDenegadosP = round((sum(Denegado, na.rm = TRUE) / nrow(t) * 100), digits=1)
+)
+tab1 <- bind_rows(tab1, tab1b)
+
+tab1 <- tab1 %>%
+    mutate(
+        order = case_when(
+            CCAA == "Total" ~ 2,
+            CCAA == "No consta" ~ 1,
+            TRUE ~ 0
+        )
+    ) %>%
+    arrange(order, CCAA) %>%
+    select(-order)
+
+t1 <- tab1 %>%
+    gt() %>%
+        tab_header(
+                title = "Tabla 1: Distribución general por CCAA"
+        ) %>%
+        cols_label(
+                CCAA = "Comunidad Autónoma",
+                TotalSolicitantesN = "Núm.",
+                TotalSolicitantesP = "%",
+                TotalPrestacionesN = "Núm.",
+                TotalPrestacionesP = "%",
+                TotalMuerteDuranteTramitacionN = "Núm.",
+                TotalMuerteDuranteTramitacionP = "%",
+                TotalDenegadosN = "Núm.",
+                TotalDenegadosP = "%"
+        ) %>%
+        tab_spanner(
+                label = "Solicitudes",
+                columns = c(TotalSolicitantesN, TotalSolicitantesP)
+        ) %>%
+        tab_spanner(
+                label = "Prestaciones",
+                columns = c(TotalPrestacionesN, TotalPrestacionesP)
+        ) %>%
+        tab_spanner(
+            label = "Muertes durante tramitación",
+            columns = c(TotalMuerteDuranteTramitacionN, TotalMuerteDuranteTramitacionP)
+        ) %>%
+        tab_spanner(
+            label = "Denegaciones",
+            columns = c(TotalDenegadosN, TotalDenegadosP)
+        ) %>%
+        cols_align(
+            align = "center",
+            columns = -CCAA
+        ) %>%
+        tab_style(
+            style = cell_borders(
+            sides = "bottom",
+            color = "black",
+            weight = px(2)
+            ),
+            locations = cells_body(
+            rows = 18
+            )
+        ) %>%
+        tab_style(
+            style = list(
+            cell_fill(color = "gray40"),
+            cell_text(color = "white")
+            ),
+            locations = cells_body(
+            rows = 19
+            )
+        ) %>%
+        tab_source_note( # Añadir nota de la fuente
+    source_note = "Nota: Todos los porcentajes se refieren al total de solicitudes."
+  ) %>%
+        gt_theme_espn()
+gtsave(t1, "script_tablas/prueba.html")
+### Tabla 2: Distribución por edad de solicitantes y beneficiarios
+
+### Tabla 3: Distribución por enfermedad de base de solicitantes y beneficiarios
+
+## Estadísticas regionales
+### Tabla 4: Distribución por CCAA de solicitantes, beneficiarios y denegaciones
+
+### Tasa de mortalidad por PAM total y por CCAA
